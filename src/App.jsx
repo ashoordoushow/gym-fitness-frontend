fe852e333b35ead63fc288eb75edffc31b5c4f2b
ashoordoushow <ashoor12323@gmail.com>
2024-11-07 19:29:52 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { ExercisesPage } from "./ExercisesPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ExercisesPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/ExercisesPage.jsx
@@ -0,0 +1,7 @@
+export function ExercisesPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
