c3ce2e5616c02c66f49685058b9a93c0ef642c3d
ashoordoushow <ashoor12323@gmail.com>
2024-11-10 16:21:52 -0600

    added syntax to main.jsx file axios request

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/" : "https://gym-fitness-api.onrender.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
