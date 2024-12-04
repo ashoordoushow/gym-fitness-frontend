545edf2ff2d425ee4dd987448ac6dea38fa7b63b
ashoordoushow <ashoor12323@gmail.com>
2024-12-04 07:10:40 -0600

    updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
-axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/" : "https://gym-fitness-api.onrender.com";
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/" : "https://built-to-conquer.up.railway.app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
